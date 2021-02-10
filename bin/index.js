7422095e882363a20c1b95301cfec8442e7315a1
Christopher Bradshaw <chris.kofox@gmail.com>
2021-02-10 15:26:26 -0700

    v1-alpha

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+import program from "commander";
+
+program
+  .command("version")
+  .description("Show what version of ori is installed")
+  .action(function showVersion() {
+    console.log(`Ori version ${process.env?.npm_package_version}`);
+  });
+
+program.parse(process.argv);
