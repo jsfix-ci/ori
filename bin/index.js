965c04aed22de76b8ee8fe71d9d32a5eecd0e1d3
Christopher Bradshaw <chris.kofox@gmail.com>
2021-02-10 15:36:26 -0700

    Get version via direct import

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-
 const program = require("commander");
+const version = require("../lib/index.js");
 
 program
   .command("version")
   .description("Show what version of ori is installed")
   .action(function showVersion() {
-    console.log(`Ori version ${process.env?.npm_package_version}`);
+    console.log(`Ori version ${version}`);
   });
 
 program.parse(process.argv);
