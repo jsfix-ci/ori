70c302bf59da4f448a2c87b89a4e71b075b94dc9
Christopher Bradshaw <chris.kofox@gmail.com>
2021-02-10 15:49:25 -0700

    Functioning version 1-alpha

--- bin/index.js
+++ bin/index.js
@@ -2,11 +2,5 @@
 const program = require("commander");
 const version = require("../lib/index.js");
 
-program
-  .command("version")
-  .description("Show what version of ori is installed")
-  .action(function showVersion() {
-    console.log(`Ori version ${version}`);
-  });
-
+program.version(version, "-v, --version");
 program.parse(process.argv);
